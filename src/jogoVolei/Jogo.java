9be0a2fc7e5fc9e9fb9ee8dda1f0f47ed704e6bb
Mikso <bmarcos330@gmail.com>
2017-07-25 16:59:09 -0300

    add 2

--- src/jogoVolei/Jogo.java
+++ src/jogoVolei/Jogo.java
@@ -3,11 +3,24 @@
  */
 package jogoVolei;
 
+
 /**
  * @author Bruno
  *
  */
 public class Jogo {
 	private int numero;
+	
+	public void iniciarJogo(){
+		
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
 	
 }
