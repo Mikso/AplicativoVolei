262f8c7188edf497f05a79f69de2cbc585f79111
Mikso <bmarcos330@gmail.com>
2017-07-25 16:29:53 -0300

    add 1

---
+++ src/main/InitApp.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package main;
+
+import jogoVolei.Jogo;
+
+/**
+ * @author Bruno
+ *
+ */
+public class InitApp {
+	public static void main(String[] args) {
+		Jogo jg = new Jogo();
+		
+	}
+}
