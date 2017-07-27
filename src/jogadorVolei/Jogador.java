697f2c290718d4a291a7d11cbb0b3931a7460848
Mikso <bmarcos330@gmail.com>
2017-07-27 11:53:44 -0300

    add 5

--- src/jogadorVolei/Jogador.java
+++ src/jogadorVolei/Jogador.java
@@ -12,16 +12,31 @@ import java.util.Date;
  */
 
 public class Jogador {
-	private String nome;
-	private Date dsnasc;
-	private int idade;
+	private String nome = null;
+	private Date dsnasc = null;
+	private int idade = 0;
 	private ArrayList<String> jogadores = new ArrayList<>();
+	private ArrayList<Integer> idades = new ArrayList<>();
+	private ArrayList<Date> datas = new ArrayList<>();
 	
-	void inserirJogador(){
+	public void inserirJogador(){
 		Jogador jg = new Jogador();
 		
-		System.out.println("Hora de informar os jogadores");
+		System.out.println("Hora de informar os jogadores, informe o primeiro player");
+		jg.setNome(nome);
+		jogadores.add(nome);
+		jg.setIdade(idade);
+		idades.add(idade);
+		jg.setDsnasc(dsnasc);
+		datas.add(dsnasc);
 		
+		System.out.println("Agora insira o segundo jogador");
+		jg.setNome(nome);
+		jogadores.add(nome);
+		jg.setIdade(idade);
+		idades.add(idade);
+		jg.setDsnasc(dsnasc);
+		datas.add(dsnasc);
 		
 	}
 	
@@ -47,9 +62,6 @@ public class Jogador {
 	}
 	public ArrayList<String> getJogadores() {
 		return jogadores;
-	}
-	public void setJogadores(ArrayList<String> jogadores) {
-		this.jogadores = jogadores;
 	}
 	
 	
